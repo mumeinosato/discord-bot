1b3061e0f11775e1fc316156b79552d6fa6894d1
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 19:31:29 +0900

    test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dislash import InteractionClient, slash_commands, Option, OptionType
 
 bot = commands.Bot(command_prefix="mu:", help_command=None)
 slash = InteractionClient(bot)
+test_guilds = [945916362872746034]
 
 with open('/home/mumeinosato/discord-bot/token.txt', 'r') as fin:
     content = fin.read()
@@ -14,6 +15,7 @@ print("起動しました")
 @slash.command(
     name="help",
     description="helpを表示します" ,
+    guild_ids=test_guilds
 )
 async def help(inter):#コマンドを定義するときの関数は必ずContextという引数が渡される。つまり引数を最低一つだけでも書いておかないと動かないので注意
     embed = discord.Embed(title="ヘルプ", description="このヘルプコマンドにはプレフィックスを書いていないため、\n実行には全て`mu:コマンド名`とする必要があります。",color=0x4169e1)
