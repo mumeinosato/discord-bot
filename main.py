dcfe143a3d248a7afa8b2daaf3948995e46cc2de
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 19:29:05 +0900

    /test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 import discord.ext
 from discord.ext import commands
-from dislash import slash_commands, Option, OptionType
+from dislash import InteractionClient, slash_commands, Option, OptionType
 
-bot = commands.Bot(command_prefix="mu:", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="mu:", help_command=None)
+slash = slash_commands.SlashClient(bot)
 
 with open('/home/mumeinosato/discord-bot/token.txt', 'r') as fin:
     content = fin.read()
