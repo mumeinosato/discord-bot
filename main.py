de65ee4574ad618b483007db4f2280e602084c74
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 18:10:44 +0900

    main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#import discord
-#import discord.ext
-#from discord.ext import commands
+import discord
+import discord.ext
+from discord.ext import commands
 
-#bot = commands.Bot(command_prefix="mu:", help_command=None)
+bot = commands.Bot(command_prefix="mu:", help_command=None)
 
 with open('/home/mumeinosato/discord-bot/token.txt', 'r') as fin:
     token = fin.read()
-print(token)
+
 print("起動しました")
 
-#bot.run("token")
+bot.run("token")
