91290df5857b98a2e9d9bdd2a2403cfe200ff3e6
BuildTools <unconfigured@null.spigotmc.org>
2022-07-13 23:28:34 +0900

    out

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 from dislash import InteractionClient, slash_commands, Option, OptionType
 from janome.tokenizer import Tokenizer
 import markovify
+import subprocess
+import time
 
 bot = commands.Bot(command_prefix="mu:", help_command=None)
 slash = InteractionClient(bot)
@@ -91,46 +93,12 @@ async def on_message(message):
             await channel.send(embed=embed)
 
     else:
-        file = '/home/mumeinosato/discord-bot/markovi.txt' 
-        f = open(file, 'r', encoding="utf-8")
-        text = f.read()
-
-        def text_split(text):
-            str_table = str.maketrans({
-                '。': '',   
-                '\n': '。',
-                '\r': '',
-                '(': '（',
-                ')': '）',
-                '[': '［',
-                ']': '］',
-                '"':'”',
-                "'":"’",
-            })
-            text = text.translate(str_table)
-            t = Tokenizer()
-            tokens = t.tokenize(text, wakati=True)
-            splitted_text_list = []
-            for i in tokens:
-                if i != '。' and i != '！' and i != '？':
-                    i += ' '
-                elif i == '。' or i == '！' or i == '？':
-                    i = '\n'
-                splitted_text_list.append(i)
-                splitted_text_str = "".join(splitted_text_list)
-
-            return splitted_text_str
-
-        splitted_text_str = text_split(text)
-        text_model_3 = markovify.NewlineText(splitted_text_str, state_size=3)
-
-        for i in range(1):
-            out = text_model_3.make_sentence(tries=100)
-            print(out)
-            #f = open('/home/mumeinosato/discord-bot/marukovi_output.txt', 'w')
-            #f.write(out)
-            #f.close()
-            await message.channel.send(out)
+        path = 'marukovi.py'
+        subprocess.call('python %s' % path)
+        time.sleep(3)
+        with open('//home/mumeinosato/discord-bot/marukovi_output.txt', 'r') as fin:
+            outget = fin.read()
+        await message.channel.send(outget)
 
     await bot.process_commands(message)
 
