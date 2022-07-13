58dbf4895b57033fb9de6be24715087c46ba0a5d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-13 23:10:25 +0900

    marukovi

--- cogs/marukovi.py
+++ cogs/marukovi.py
@@ -37,7 +37,8 @@ splitted_text_str = text_split(text)
 text_model_3 = markovify.NewlineText(splitted_text_str, state_size=3)
 
 for i in range(1):
-    print(text_model_3.make_sentence(tries=100))
-    #f = open('/home/mumeinosato/discord-bot/marukovi_output.txt', 'w')
-    #f.write(text_model_3.make_sentence(tries=100))
-    #f.close()
+    out = text_model_3.make_sentence(tries=100)
+    print(out)
+    f = open('/home/mumeinosato/discord-bot/marukovi_output.txt', 'w')
+    f.write(out)
+    f.close()
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 import discord.ext
 from discord.ext import commands
 from dislash import InteractionClient, slash_commands, Option, OptionType
-import random
-#import traceback
+from janome.tokenizer import Tokenizer
+import markovify
 
 bot = commands.Bot(command_prefix="mu:", help_command=None)
 slash = InteractionClient(bot)
@@ -89,11 +89,46 @@ async def on_message(message):
             await channel.send(embed=embed)
 
     else:
-        with open('/home/mumeinosato/discord-bot/aitext.txt', 'r') as fin:
-            aisel = fin.read()
-        print(aisel)
-        seltxt = [aisel]
-        await message.channel.send(random.choice(seltxt))
+        file = '/home/mumeinosato/discord-bot/markovi.txt' 
+        f = open(file, 'r', encoding="utf-8")
+        text = f.read()
+
+        def text_split(text):
+            str_table = str.maketrans({
+                '。': '',   
+                '\n': '。',
+                '\r': '',
+                '(': '（',
+                ')': '）',
+                '[': '［',
+                ']': '］',
+                '"':'”',
+                "'":"’",
+            })
+            text = text.translate(str_table)
+            t = Tokenizer()
+            tokens = t.tokenize(text, wakati=True)
+            splitted_text_list = []
+            for i in tokens:
+                if i != '。' and i != '！' and i != '？':
+                    i += ' '
+                elif i == '。' or i == '！' or i == '？':
+                    i = '\n'
+                splitted_text_list.append(i)
+                splitted_text_str = "".join(splitted_text_list)
+
+            return splitted_text_str
+
+        splitted_text_str = text_split(text)
+        text_model_3 = markovify.NewlineText(splitted_text_str, state_size=3)
+
+        for i in range(1):
+            out = text_model_3.make_sentence(tries=100)
+            print(out)
+            #f = open('/home/mumeinosato/discord-bot/marukovi_output.txt', 'w')
+            #f.write(out)
+            #f.close()
+            await message.channel.send(out)
 
     await bot.process_commands(message)
 
