392df2a35d31daa17b0e93092338e30e3d5c685f
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2023-07-27 21:48:23 +0900

    天気予報

--- cogs/weather.py
+++ cogs/weather.py
@@ -20,12 +20,13 @@ async def weather(self, inter: discord.Interaction, sid: str):
         url = api + "?" + params
         data = urllib.request.urlopen(url).read()
         text = data.decode("utf-8")
-        ##embed = discord.Embed(title="ヘルプ", description="すべてスラッシュコマンドに対応する予定です", color=0x4169e1)
-        ##embed.add_field(name="**mcid**", value="Minecraftのプレイヤーを検索します", inline=False)
-        ##await inter.repone.send_message(embed=embed)
+        embed = discord.Embed(title=text["title"], description=text["publishingOffice"], color=0x4169e1)
+        embed.add_field(name="**今日**", value=text["forecasts"][0]["telop"] + "\n 風向:" + text["forecasts"][0]["detail"]["wind"] + "  風量:" + text["forecasts"][0]["detail"]["wave"] , inline=False)
+        embed.add_field(name="**明日**", value=text["forecasts"][1]["telop"] + "\n 風向:" + text["forecasts"][1]["detail"]["wind"] + "  風量:" + text["forecasts"][1]["detail"]["wave"] , inline=False)
+        await inter.repone.send_message(embed=embed)
     else:
         embed = discord.Embed(title="エラー", description="エラーが発生しました", color=0x4169e1)
-        embed.add_field(name="処理できる地域を指定してください", value="解決方法は現在準備中です", inline=False)
+        embed.add_field(name="処理できる地域を指定してください", value="[利用可能な都市を調べる](https://raw.githubusercontent.com/mumeinosato/discord-bot/main/cogs/citycheck.py)", inline=False)
         await inter.response.send_message(embed=embed)   
 
 async def setup(bot):
